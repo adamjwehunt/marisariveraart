bf22f1b1871d3e64a07a2a4a05e2aad12ef7edba
ajwehunt <adamjwehunt@gmail.com>
2018-08-25 14:52:02 -0700

    Set up less

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SidebarLeft from './components/SidebarLeft';
 import Main from './components/Main';
 import SidebarRight from './components/SidebarRight';
 import Footer from './components/Footer';
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
