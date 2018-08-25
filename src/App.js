988cd6da9cf1001654fbb2d867f29cd7e0b8ec61
ajwehunt <adamjwehunt@gmail.com>
2018-08-25 13:06:57 -0700

    Add css grid, content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App grid-container">
+        <div className="sidebar-left">
+        </div>
+        <div className="sidebar-right">
+        </div>
+        <div className="header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Marisa Rivera Art</h1>
+        </div>
+        <div className="footer">
+        </div>
+        <div className="main">
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
